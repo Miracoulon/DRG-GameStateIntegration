ab3239795b8a79e31d6824bf3b7385741a5cb079
Miracoulon <35643514+Miracoulon@users.noreply.github.com>
2024-02-29 22:32:41 +0100

    Added missing export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,4 @@
-export { DRGGSI } from './DRGGSI';
+export { DRGGSI, DRGGSIOptions } from './DRGGSI';
 export { DRGGSIMission } from './DRGGSIMission';
+export { DRGGSIPlayer } from './Player/DRGGSIPlayer';
+export { EPlayerState } from './Player/EPlayerState';
